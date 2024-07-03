4dbf44fd05cbeaf2b52a9d922c88c1eca9502339
hanumfnr <hanumfaulinnuur@gmail.com>
2024-07-03 07:24:11 +0700

    feat:create a feature search data guru

--- resources/views/Guru/listGuru.blade.php
+++ resources/views/Guru/listGuru.blade.php
@@ -9,7 +9,11 @@
          <div class="alert alert-info mt-3">{{ Session('success') }}</div>
        @endif
           <h5 class="card-title">List Dewan Guru SDN 1 Sambimulyo</h5><hr>
-          <a class="btn btn-success mb-3" href="{{ url('/export-excel') }}" role="button"><i class="bi bi-file-earmark-spreadsheet-fill"></i> Ekspor Excel</a>
+          <a class="btn btn-success mb-3" href="{{ url('/export-excel') }}" role="button"><i class="bi bi-file-earmark-spreadsheet-fill"></i> Ekspor Excel</a><br>
+          <form class="d-inline-flex mt-2 mb-4" action="{{ url('/cari/guru') }}" method="GET" >
+            <input class="form-control me-2" type="text" name="query" placeholder="Cari Nama Guru" aria-label="Search">
+            <button class="btn btn-info" type="submit"><i class="bi bi-search"></i></button>
+          </form>
           <table class="table table-striped table-bordered">
             <thead>
               <tr class="text-center">
@@ -23,6 +27,9 @@
                 <th scope="col">Lihat Detail</th>
               </tr>
             </thead>
+            @if($guru->isEmpty())
+          <p class="text-danger">Data Yang Anda Cari Tidak Ditemukan ! <a href="{{ url('guru') }}" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><b>Klik Untuk Kembali</b></a></p>
+           @else
             @foreach ($guru as $key=> $g)
             <tbody>
               <tr>
@@ -44,6 +51,7 @@
                 </td>
               </tr>
             @endforeach
+            @endif
             </tbody>
           </table>
         </div>
